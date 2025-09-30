1ef104f119508e37857c598f8004a0c2e85a4411
ekwe7 <ekwea753@gmail.com>
2025-09-30 08:41:12 +0100

    number counter

--- java/GateOne/NumberCounter.java
+++ java/GateOne/NumberCounter.java
@@ -7,7 +7,7 @@ Scanner scanner = new Scanner(System.in);
 int positiveNumber = 0;
 int negativeNumber = 0;
 int zeroNumber = 0;
-char charater;
+
 
 do{
 System.out.print("Enter a number: ");
@@ -20,10 +20,6 @@ negativeNumber ++;
 }else{
 zeroNumber++;
 }
-
-System.out.print("Do you want to continue? (y/n): ");
-charater = scanner.next().charAt(0);
-}while (charater == 'y' || charater == 'Y');
 
 System.out.println("Number of positive number is: " + positiveNumber);
 System.out.println("Number of Negative Number is: " + negativeNumber );
