bff8f7f27c21633aa02da44622eef4b87d2aa964
ekwe7 <ekwea753@gmail.com>
2025-10-06 20:17:13 +0100

    Functions

---
+++ java/Kata/Kata.java
@@ -0,0 +1,17 @@
+public class Kata{
+
+
+	public static boolean isEven(int number){
+	
+	if(number % 2==0){
+		return true;
+	  }
+	else if(number % 2 != 0){
+		return false;
+	  }
+	  return true;
+	}
+
+
+
+}
