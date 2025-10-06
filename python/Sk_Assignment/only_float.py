92033022cdeded79fa08086c89ac7179cf266698
ekwe7 <ekwea753@gmail.com>
2025-10-06 22:32:31 +0100

    Add divide_or_square.py and only_float.py assignments

---
+++ python/Sk_Assignment/only_float.py
@@ -0,0 +1,15 @@
+
+def only_float(a, b):
+    if isinstance(a, float) and isinstance(b, float):
+        return 2
+    elif isinstance(a, float) or isinstance(b, float):
+        return 1
+    else:
+        return 0
+
+
+number1 = float(input("Enter number one: "));
+number2 = float(input("Enter number two: "));
+output = number1, number2
+
+print(output);
