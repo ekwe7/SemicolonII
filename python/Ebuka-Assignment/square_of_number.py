7a51ff2d54ebc57f2155b47f8b67ec86f8f4f47d
ekwe7 <ekwea753@gmail.com>
2025-10-07 00:22:41 +0100

    traffic light and square of number

---
+++ python/Ebuka-Assignment/square_of_number.py
@@ -0,0 +1,5 @@
+
+for number in range(1, 6):
+	square_of_number = number ** 2;
+	
+	print(square_of_number);
---
+++ python/Ebuka-Assignment/traffic_light.py
@@ -0,0 +1,11 @@
+color = input("Enter the color (red, green, or yellow): ");
+user_color = color.lower();
+
+if (user_color== "green"):
+	print("Go")
+elif (user_color== "red"):
+	print("Stop")
+elif (user_color== "yellow"):
+	print("Wait")
+else:
+	print("Invalid color, Please enter correct color")
