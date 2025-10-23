9daab6012be42d9b5194f7ef3f5a761b4194003d
ekwe7 <ekwea753@gmail.com>
2025-10-23 09:10:01 +0100

    Daily task

---
+++ DailyTask/ConvertStringToUpper.java
@@ -0,0 +1,10 @@
+public class ConvertStringToUpper{
+public static String stringIsUpperCase(String word){
+	
+	String myWord = word.toUpperCase();
+	
+	return myWord;	
+	}
+
+
+}
---
+++ DailyTask/StringIsUpperTest.java
@@ -0,0 +1,21 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class StringIsUpperTest{
+
+	@Test
+	public void testThatStringIsEmpty(){
+	
+		ConvertStringToUpper stringUpper = new ConvertStringToUpper();
+		
+		String expected = "SEMICOLON";
+		String result = stringUpper.stringIsUpperCase("SEMICOLON");
+		
+		assertEquals(expected, result);
+	
+	}
+	
+	
+}
