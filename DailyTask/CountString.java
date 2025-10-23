744129896076e65e40d97df5d469ee27b650fa1b
ekwe7 <ekwea753@gmail.com>
2025-10-23 08:13:08 +0100

    Daily task

---
+++ DailyTask/CountString.java
@@ -0,0 +1,12 @@
+public class CountString{
+public static String stringCount(String word){
+	String[] myWords = word.split(" ");
+	
+	for(int count = 0; count < myWords.length; count++){
+		System.out.print(myWord[count]);
+	}
+	
+	}
+
+
+}
