d7c361a78f2248bcea1ce64730a3fe07fe364509
ekwe7 <ekwea753@gmail.com>
2025-10-28 11:16:39 +0100

    phase gate

---
+++ phaseGate/java/DepreciationOfValue.java
@@ -0,0 +1,19 @@
+
+public class DepreciationOfValue{
+
+public int yearDepreciation(double NumberOfItems, double price){
+	
+	double fixedPrice = 50000;
+	itn percentage = 8;
+	double result = 0;	
+	
+	while(numberOfItems < 0){
+		double output = NumberOfItems - price
+			
+		result = (fixedPrice - output) * percentage % 100;
+		return result;
+		
+		}
+}
+
+}
---
+++ phaseGate/java/DepreciationOfValueTest.java
@@ -0,0 +1,24 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DepreciationOfValueTest{
+
+
+	@Test
+	public void testThatPriceDepreciateByEightPercent(){
+	
+	myCalculation = new DepreciationOfValue();
+	
+	double result =  myCalculation.yearDepreciation();
+	
+	assertEquals(8, result);
+	
+	
+	}
+
+	
+
+
+
+}
